85463c9d0537538f159fe7288f460d025ed5f5e7
Jérôme <jerome.villafruela@gmail.com>
2017-11-13 00:04:08 +0100

    Heritage id examples for fr

--- heritage.php
+++ heritage.php
@@ -9,11 +9,19 @@ class Heritage {
     public function getHeritageId($text) {
         return $this->country == 'fr' ? $this->getHeritageIdfr($text) : FALSE;
     }
-
+    
+    /*  Examples
+        {{Mérimée}}    
+        {{Mérimée|PA00090753}}    
+        {{Mérimée|PA00090676|IA35024928}}
+        {{Mérimée|type=inscrit|PA16000036}}    
+        {{mérimée|type=classé|PA16000036}}    
+        {{Mérimée|type=classé+inscrit|PA00110393}}
+     */
     protected function getHeritageIdfr($text) {
         if (preg_match('/\{\{Mérimée\|(.+)\}\}/i', $text, $matches, PREG_OFFSET_CAPTURE) !== 1) return false;
         $tplparams=$matches[1][0];
-        if (preg_match('/PA\d[0-9AB]\d\d\d\d\d\d/', $text, $matches, PREG_OFFSET_CAPTURE) !== 1) return false;
+        if (preg_match('/PA\d[0-9AB]\d\d\d\d\d\d/', $tplparams, $matches, PREG_OFFSET_CAPTURE) !== 1) return false;
         //print_r($matches);
         return $matches[0][0];   
     }
